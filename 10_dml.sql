172d00c3438f050440b07d06aa62c05dd3f4db69
DESKTOP-OSFKUPN <epyneoz27@gmail.com>
2024-09-25 16:37:04 +0900

    24.09.25 2

--- 10_dml.sql
+++ 10_dml.sql
@@ -46,3 +46,41 @@ select menu_code, category_code
 update tbl_menu
    set category_code = 7
  where menu_code = '23';
+ 
+ 
+ -- subquery를 사용해서 update
+ -- 주의점 : update나 delete시 자기 자신의 테이블의 데이터를 사용하게 되면 1093 error 발생한다.
+ 
+ -- 문제.
+ -- 메뉴의 이름이 파인애플탕인 메뉴의 카테고리코드를 6으로 수정하시오.
+ -- where menu_name = '파인애플탕' x
+ -- where menu_code를 통해 파인애플탕 추론하기
+update tbl_menu
+set category_code = 6
+where menu_code = (select cte.menu_code from (select menu_code from tbl_menu where menu_name = '파인애플탕') cte);
+
+
+-- delete
+-- 테이블의 행을 삭제하는 구문이다.
+-- 테이블의 행의 갯수가 줄어든다.
+
+-- limit
+delete from tbl_menu
+order by menu_price -- asc (기본으로 들어있음)
+limit 2;
+
+-- where 사용으로 단일 행 삭제
+delete 
+from tbl_menu
+where menu_code = 23;
+
+delete from tbl_menu;
+
+-- replace
+-- insert 시 primary key 또는 unique key가 충돌이 발생한다면 replace를 통해 중복된 데이터를 덮어쓸 수 있다.
+
+insert into tbl_menu values(15, '소주', 6000, 10, 'Y');
+replace into tbl_menu values(15, '소주', 7000, 10, 'Y');
+
+
+select * from tbl_menu;
